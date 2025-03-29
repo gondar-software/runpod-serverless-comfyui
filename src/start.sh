48d4db8d7ca9d868cc859d5a57a39c4e2b090398
root <root@ip-172-31-18-98.us-east-2.compute.internal>
2025-03-29 19:48:03 +0000

    update

--- src/start.sh
+++ src/start.sh
@@ -3,17 +3,18 @@
 # Use libtcmalloc for better memory management
 TCMALLOC="$(ldconfig -p | grep -Po "libtcmalloc.so.\d" | head -n 1)"
 export LD_PRELOAD="${TCMALLOC}"
+ln -s /runpod-volume /workspace
 
 # Serve the API and don't shutdown the container
 if [ "$SERVE_API_LOCALLY" == "true" ]; then
     echo "runpod-worker-comfy: Starting ComfyUI"
-    python3 /comfyui/main.py --disable-auto-launch --disable-metadata --listen &
+    /workspace/ComfyUI/venv/bin/python /workspace/ComfyUI/main.py --disable-auto-launch --disable-metadata --listen &
 
     echo "runpod-worker-comfy: Starting RunPod Handler"
     python3 -u /rp_handler.py --rp_serve_api --rp_api_host=0.0.0.0
 else
     echo "runpod-worker-comfy: Starting ComfyUI"
-    python3 /comfyui/main.py --disable-auto-launch --disable-metadata &
+    /workspace/ComfyUI/venv/bin/python /workspace/ComfyUI/main.py --disable-auto-launch --disable-metadata &
 
     echo "runpod-worker-comfy: Starting RunPod Handler"
     python3 -u /rp_handler.py
