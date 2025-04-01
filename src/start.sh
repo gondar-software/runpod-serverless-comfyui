b32ecc774aae5a22a9f8382da8c32b21bafa7bd6
gondar-software <monate615@gmail.com>
2025-04-01 20:00:29 +0000

    update start.sh

--- src/start.sh
+++ src/start.sh
@@ -11,15 +11,15 @@ mkdir -p "$COMFY_OUTPUT_PATH"
 if [ "$SERVE_API_LOCALLY" == "true" ]; then
     echo "runpod-worker-comfy: Starting ComfyUI"
     /workspace/ComfyUI/venv/bin/python /workspace/ComfyUI/main.py --disable-auto-launch --disable-metadata --listen --output-directory "$COMFY_OUTPUT_PATH" &
+    python3 /init.py &
 
     echo "runpod-worker-comfy: Starting RunPod Handler"
     python3 -u /rp_handler.py --rp_serve_api --rp_api_host=0.0.0.0
-    python3 /init.py
 else
     echo "runpod-worker-comfy: Starting ComfyUI"
     /workspace/ComfyUI/venv/bin/python /workspace/ComfyUI/main.py --disable-auto-launch --disable-metadata --output-directory "$COMFY_OUTPUT_PATH" &
+    python3 /init.py &
 
     echo "runpod-worker-comfy: Starting RunPod Handler"
     python3 -u /rp_handler.py
-    python3 /init.py
 fi
